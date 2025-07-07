d2d0de6c62160374469aa5db50b81dd535bdd2b6
Sangram <sangrammohanty73@gmail.com>
2025-07-07 08:41:55 +0530

    second commit

---
+++ MyApp/src/main/java/com/example/demo/MyAppApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppApplication.class, args);
+	System.out.println("Hello World");
+	System.out.println("Hello World 123");
+	
+	}
+
+}
